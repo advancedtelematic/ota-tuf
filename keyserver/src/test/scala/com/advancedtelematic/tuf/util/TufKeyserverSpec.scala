5bb130c60b5bdf03d39744a5581244a578970054
Simão Mata <simao@advancedtelematic.com>
2017-05-04 11:27:05 +0200

    Periodically renew vault tokens

--- keyserver/src/test/scala/com/advancedtelematic/tuf/util/TufKeyserverSpec.scala
+++ keyserver/src/test/scala/com/advancedtelematic/tuf/util/TufKeyserverSpec.scala
@@ -1,38 +1,51 @@
 package com.advancedtelematic.tuf.util
 
 import java.security.Security
+import java.time.Instant
 import java.util.concurrent.ConcurrentHashMap
 
+import akka.http.scaladsl.util.FastFuture
+import com.advancedtelematic.libtuf.data.TufDataType.KeyId
 import com.advancedtelematic.tuf.keyserver.Settings
 import com.advancedtelematic.tuf.keyserver.vault.VaultClient
 import com.advancedtelematic.tuf.keyserver.vault.VaultClient.{VaultKey, VaultKeyNotFound}
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSuite, Matchers}
-import com.advancedtelematic.libtuf.data.TufDataType.KeyId
 
+import scala.collection.mutable
 import scala.concurrent.Future
 
+class FakeVault extends VaultClient {
+  private val keys = new ConcurrentHashMap[KeyId, VaultKey]
+  val renewedTimestamps = mutable.MutableList[Instant]()
 
-abstract class TufKeyserverSpec extends FunSuite with Matchers with ScalaFutures with Settings {
+  override def createKey(key: VaultKey): Future[Unit] = {
+    keys.put(key.id, key)
+    Future.successful(())
+  }
 
-  Security.addProvider(new BouncyCastleProvider())
-
-  val fakeVault = new VaultClient {
-    private val keys = new ConcurrentHashMap[KeyId, VaultKey]
-
-    override def createKey(key: VaultKey): Future[Unit] = {
-      keys.put(key.id, key)
-      Future.successful(())
+  override def findKey(keyId: KeyId): Future[VaultKey] =
+    Option(keys.get(keyId)) match {
+      case Some(k) => Future.successful(k)
+      case None => Future.failed(VaultKeyNotFound)
     }
 
-    override def findKey(keyId: KeyId): Future[VaultKey] =
-      Option(keys.get(keyId)) match {
-        case Some(k) => Future.successful(k)
-        case None => Future.failed(VaultKeyNotFound)
-      }
+  override def deleteKey(keyId: KeyId): Future[Unit] =
+    Future.successful(keys.remove(keyId))
 
-    override def deleteKey(keyId: KeyId): Future[Unit] =
-      Future.successful(keys.remove(keyId))
+  override def renewToken(): Future[Unit] = {
+    this.synchronized {
+      Instant.now +=: renewedTimestamps
+    }
+    FastFuture.successful(())
   }
+
+  def lastRenew: Option[Instant] = this.synchronized { renewedTimestamps.headOption }
+}
+
+abstract class TufKeyserverSpec extends FunSuite with Matchers with ScalaFutures with Settings {
+  Security.addProvider(new BouncyCastleProvider())
+
+  val fakeVault = new FakeVault
 }
