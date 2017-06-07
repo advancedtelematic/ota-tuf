5a914295b0a957fc19451c96438d7cd23d796446
Simão Mata <simao@advancedtelematic.com>
2017-06-07 14:42:26 +0200

    Replace \n by real new line in json

--- libtuf/src/main/scala/com/advancedtelematic/libtuf/crypt/CanonicalJson.scala
+++ libtuf/src/main/scala/com/advancedtelematic/libtuf/crypt/CanonicalJson.scala
@@ -3,10 +3,12 @@ package com.advancedtelematic.libtuf.crypt
 import io.circe.{Json, JsonObject}
 import io.circe.syntax._
 
+import scala.collection.SortedMap
+
 object CanonicalJson {
 
   implicit class ToCanonicalJsonOps(value: Json) {
-    def canonical: String = generate(value).noSpaces
+    def canonical: String = generate(value).noSpaces.replace("\\n", "\n")
   }
 
   private def generate(value: Json): Json =
