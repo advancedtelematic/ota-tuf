8495537463f7062be03ee47847e34a6ea8c78a30
Robert Danitz <robert@advancedtelematic.com>
2017-07-07 10:45:43 +0200

    error handler + handling of repo conflicts
    - … for reposerver client
    - distinguish between {repo|key}server remote errors

--- libtuf/src/main/scala/com/advancedtelematic/libtuf/reposerver/ReposerverClient.scala
+++ libtuf/src/main/scala/com/advancedtelematic/libtuf/reposerver/ReposerverClient.scala
@@ -41,7 +41,8 @@ object ReposerverClient {
 }
 
 trait ReposerverClient {
-  protected def KeyStoreError(msg: String) = RawError(ErrorCode("reposerver_remote_error"), StatusCodes.BadGateway, msg)
+  protected def ReposerverError(msg: String) = RawError(ErrorCode("reposerver_remote_error"), StatusCodes.BadGateway, msg)
+  val RepoConflict   = RawError(ErrorCode("repo_conflict"), StatusCodes.Conflict, "repo already exists")
 
   def createRoot(namespace: Namespace): Future[RepoId]
 
@@ -73,7 +74,10 @@ class ReposerverHttpClient(reposerverUri: Uri)
 
   override def createRoot(namespace: Namespace): Future[RepoId] = {
     val req = HttpRequest(HttpMethods.POST, uri = apiUri(Path("user_repo")))
-    execHttp[RepoId](namespace, req)
+    execHttp[RepoId](namespace, req) {
+      case response if response.status == StatusCodes.Conflict =>
+        Future.failed(RepoConflict)
+    }
   }
 
   override def addTarget(namespace: Namespace, fileName: String,
@@ -90,20 +94,26 @@ class ReposerverHttpClient(reposerverUri: Uri)
       uri = apiUri(Path("user_repo") / "targets" / fileName),
       entity = entity)
 
-    execHttp[NoContent](namespace, req).map(_ => ())
+    execHttp[NoContent](namespace, req)().map(_ => ())
   }
 
   private def payloadFrom(uri: Uri, checksum: Checksum, length: Int, name: Option[TargetName],
                           version: Option[TargetVersion], hardwareIds: Seq[HardwareIdentifier], targetFormat: TargetFormat): Json =
     RequestTargetItem(uri, checksum, Some(targetFormat), name, version, hardwareIds, length).asJson
 
+  private def defaultErrorHandler[T](): PartialFunction[HttpResponse, Future[T]] = PartialFunction.empty
+
   private def execHttp[T : ClassTag](namespace: Namespace, request: HttpRequest)
+                                    (errorHandler: PartialFunction[HttpResponse, Future[T]] = defaultErrorHandler())
                                     (implicit um: FromEntityUnmarshaller[T]): Future[T] = {
     _http.singleRequest(request.withHeaders(RawHeader("x-ats-namespace", namespace.get))).flatMap {
       case r @ HttpResponse(status, _, _, _) if status.isSuccess() =>
         um(r.entity)
       case r =>
-          FastFuture.failed(KeyStoreError(s"Unexpected response from RoleKeyStore: $r"))
+        if(errorHandler.isDefinedAt(r))
+          errorHandler(r)
+        else
+          FastFuture.failed(ReposerverError(s"Unexpected response from Reposerver: $r"))
     }
-  }
+ }
 }
