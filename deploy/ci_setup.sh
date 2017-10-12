e68135bb26851f1ba2e814a267418e7c9403254a
Simão Mata <simao@advancedtelematic.com>
2017-10-12 17:30:15 +0200

    sleep more

--- deploy/ci_setup.sh
+++ deploy/ci_setup.sh
@@ -31,7 +31,7 @@ docker run -d \
 
 function docker_vault() {
     id=$(docker ps | grep tuf-vault | awk {'print $1'})
-    sleep 3
+    sleep 5
     docker exec \
            -e VAULT_TOKEN=$VAULT_TOKEN \
            -e VAULT_ADDR='http://0.0.0.0:8200' $id vault $*
