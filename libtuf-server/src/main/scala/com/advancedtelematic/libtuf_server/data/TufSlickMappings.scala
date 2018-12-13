64534672716861698e4a44243badc79ed16b1a51
Simão Mata <simao.mata@here.com>
2018-12-13 14:25:45 +0100

    Use net.i2p.crypto.eddsa for eddsa keys

--- libtuf-server/src/main/scala/com/advancedtelematic/libtuf_server/data/TufSlickMappings.scala
+++ libtuf-server/src/main/scala/com/advancedtelematic/libtuf_server/data/TufSlickMappings.scala
@@ -6,7 +6,7 @@ import com.advancedtelematic.libats.data.DataType.Checksum
 import com.advancedtelematic.libtuf.crypt.TufCrypto
 import com.advancedtelematic.libtuf.crypt.TufCrypto.KeyOps
 import com.advancedtelematic.libtuf.data.ClientDataType.TargetCustom
-import com.advancedtelematic.libtuf.data.TufDataType.{EcPrime256KeyType, Ed25519KeyType, KeyType, RoleType, RsaKeyType, JsonSignedPayload, TufKey, TufPrivateKey}
+import com.advancedtelematic.libtuf.data.TufDataType.{EcPrime256KeyType, Ed25519KeyType, Ed25519TufKey, Ed25519TufPrivateKey, JsonSignedPayload, KeyType, RoleType, RsaKeyType, TufKey, TufPrivateKey}
 import com.advancedtelematic.libtuf.data.ClientCodecs._
 import com.advancedtelematic.libtuf.data.TufCodecs._
 import com.advancedtelematic.libats.slick.db.{SlickCirceMapper, SlickEncryptedColumn}
@@ -14,13 +14,28 @@ import io.circe.Json
 import slick.jdbc.MySQLProfile.api._
 import com.advancedtelematic.libats.codecs.CirceAts._
 import com.advancedtelematic.libats.slick.codecs.SlickEnumMapper
+import net.i2p.crypto.eddsa.EdDSAPublicKey
+
+import scala.util.{Failure, Success}
 
 object TufSlickMappings {
 
   // TODO: Use getEncoded | base64 instead of pem. Add migration. Probably use TufKey if possible
+  // TODO:SM Fix this mapping
+
   implicit val publicKeyMapper = MappedColumnType.base[PublicKey, String](
-    {publicKey => publicKey.toPem},
-    {str => TufCrypto.parsePublicPem(str).get}
+    {
+      case edk: EdDSAPublicKey =>
+        Ed25519KeyType.crypto.encode(Ed25519TufKey(edk)).noSpaces
+      case publicKey => publicKey.toPem
+    },
+    {str =>
+      io.circe.jawn.parse(str).flatMap(_.as[String]).toTry.flatMap(Ed25519KeyType.crypto.parsePublic) match {
+        case Success(oi) => oi.keyval
+        case Failure(ex) =>
+          TufCrypto.parsePublicPem(str).get
+      }
+    }
   )
 
   implicit val keyTypeMapper = MappedColumnType.base[KeyType, String](
