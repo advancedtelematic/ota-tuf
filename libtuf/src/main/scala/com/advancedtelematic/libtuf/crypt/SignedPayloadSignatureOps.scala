863f9116a9bcbbd0481d87e3cf6d9fa8d58b3f6a
Simão Mata <simao.mata@here.com>
2019-05-14 18:22:37 +0200

    wip

--- libtuf/src/main/scala/com/advancedtelematic/libtuf/crypt/SignedPayloadSignatureOps.scala
+++ libtuf/src/main/scala/com/advancedtelematic/libtuf/crypt/SignedPayloadSignatureOps.scala
@@ -1,7 +1,5 @@
 package com.advancedtelematic.libtuf.crypt
 
-import java.security.InvalidKeyException
-
 import com.advancedtelematic.libtuf.data.TufDataType.{SignedPayload, TufKey}
 import io.circe.Encoder
 import org.slf4j.LoggerFactory
@@ -14,13 +12,7 @@ object SignedPayloadSignatureOps  {
   implicit class SignedPayloadSignatureOps[T : Encoder](value: SignedPayload[T]) {
     def isValidFor(tufKey: TufKey): Boolean =
       value.signatures.exists { sig =>
-        try
-          TufCrypto.isValid(sig, tufKey, value.json)
-        catch {
-          case ec: InvalidKeyException =>
-            _log.debug(s"invalid signature for key ${tufKey.id}", ec)
-            false
-        }
+        TufCrypto.isValid(sig, tufKey, value.json)
       }
   }
 }
