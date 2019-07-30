eb18b1242b97968f2ac8fbdda8e2b9fb49105d33
Simão Mata <simao.mata@here.com>
2019-07-30 11:51:53 +0200

    upgrade alpine
    Signed-off-by: Simão Mata <simao.mata@here.com>

--- project/Packaging.scala
+++ project/Packaging.scala
@@ -19,7 +19,7 @@ object Packaging {
       defaultLinuxInstallLocation in Docker := s"/opt/${moduleName.value}",
 
       dockerCommands := Seq(
-        Cmd("FROM", "advancedtelematic/alpine-jre:8"),
+        Cmd("FROM", "advancedtelematic/alpine-jre:adoptopenjdk-jdk8u222"),
         ExecCmd("RUN", "mkdir", "-p", s"/var/log/${moduleName.value}"),
         Cmd("ADD", "opt /opt"),
         Cmd("WORKDIR", s"/opt/${moduleName.value}"),
